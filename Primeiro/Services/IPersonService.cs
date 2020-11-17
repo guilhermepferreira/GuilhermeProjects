ee4e3f510df07bb9ab87fe3db84828f28831c41e
DESKTOP-DCIB305\guilh <guilhermep.ferreiraa@gmail.com>
2020-11-17 16:57:49 -0300

    PausaAdicionar arquivos de projeto.

---
+++ Primeiro/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Primeiro.Model;
+
+namespace Primeiro.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        void Delete(long id);
+    }
+}
