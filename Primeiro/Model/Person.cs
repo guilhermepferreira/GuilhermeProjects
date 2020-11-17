0fee786b864826786b3cbfb41772fcebf6338d38
DESKTOP-DCIB305\guilh <guilhermep.ferreiraa@gmail.com>
2020-11-17 16:57:49 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Primeiro.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public long FirstName { get; set; }
+        public long LastName { get; set; }
+        public long Address { get; set; }
+        public long Gender { get; set; }
+    }
+}
